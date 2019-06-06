ce5fc3f9cc324edfb1461202cd50d6718eacaa5c
ComposeC <dumeii@foxmail.com>
2019-06-06 19:41:47 +0800

    移动文件夹
    删除.gitattributes lfs传输

--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -1,18 +1,18 @@
 /****************************************************************************
  Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
+
  http://www.cocos2d-x.org
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,29 +22,29 @@
  THE SOFTWARE.
  ****************************************************************************/
 
-#ifndef __HELLOWORLD_SCENE_H__
-#define __HELLOWORLD_SCENE_H__
+#ifndef __GAME_SCENE_H__
+#define __GAME_SCENE_H__
 
 
 #include "cocos2d.h"
 
 
-class HelloWorld : public cocos2d::Layer
+class GameScene : public cocos2d::Layer
 {
-	
-public:
-    static cocos2d::Scene* createScene();
 
-    virtual bool init();
-    
-    // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
+public:
+	static cocos2d::Scene* createScene();
+
+	virtual bool init();
+
+	// a selector callback
+	void menuCloseCallback(cocos2d::Ref* pSender);
 	virtual void update(float dt);
 
-    // implement the "static create()" method manually
-    CREATE_FUNC(HelloWorld);
+	// implement the "static create()" method manually
+	CREATE_FUNC(GameScene);
 
-	
+
 	//坐标转换，opgl坐标转换为Tiled地图坐标（每块小地图的坐标）
 	cocos2d::Vec2 toTileCoord(cocos2d::Vec2 position);
 	cocos2d::Vec2 toScreenCoord(cocos2d::Vec2 position);
@@ -63,3 +63,4 @@ private:
 
 
 #endif // __HELLOWORLD_SCENE_H__
+
