147bee0f0fcc69a706e820c224ef63a22438a11a
Owarimasu <401205991@qq.com>
2019-06-08 17:25:31 +0800

    skill Portrait
    skillPortrait and keyListen

--- Classes/TollgateScene.cpp
+++ Classes/TollgateScene.cpp
@@ -85,8 +85,7 @@ bool TollgateScene::init()
 	////////////////////////////
 	//添加技能图标与冷却
 	auto skillItem = CCDirector::sharedDirector()->getWinSize();
-	
-	auto menuSkillButton = SkillButton::create("SkillPortrait/Ashe.png", "SkillPortrait/Ashe3.png", 2.f);  //(naomal,cool,time)
+	auto menuSkillButton = SkillButton::create("SkillPortrait/Ashe.png", "SkillPortrait/Ashe3.png", 2.f);  //(normal,cool,time)
 	menuSkillButton->setPosition(skillItem.width / 2, skillItem.height / 2);
 	this->addChild(menuSkillButton);
 
