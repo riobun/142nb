18cd4b63c83676af3bff80369e3f2247157041f2
riobun <49815645+riobun@users.noreply.github.com>
2019-06-09 13:18:57 +0800

    Csv文件读取

--- Classes/CsvData.h
+++ Classes/CsvData.h
@@ -1,1 +1,27 @@
-#pragma once
+#ifndef CsvData_H
+#define CsvData_H
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class CsvData : public Ref
+{
+public:
+	CREATE_FUNC(CsvData);
+	virtual bool init();
+
+	/* 添加一行数据 */
+	void addLineData(ValueVector lineData);
+
+	/* 获取某行的数据 */
+	ValueVector getSingleLineData(int iLine);
+
+	/* 获取行列大小 */
+	Size getRowColNum();
+protected:
+private:
+	/* 存放Csv文件所有行的数据，试试这样理解：ValueVector<Value<ValueVector>) */
+	ValueVector m_allLinesVec;
+};
+
+#endif
--- Classes/CsvUtil.h
+++ Classes/CsvUtil.h
@@ -1,1 +1,47 @@
-#pragma once
+
+#ifndef CsvUtil_H
+#define CsvUtil_H
+
+#include "cocos2d.h"
+#include "CsvData.h"
+
+using namespace cocos2d;
+
+class CsvUtil : public Ref {
+public:
+	/* 获取工具对象 */
+	static CsvUtil* getInstance();
+
+	virtual bool init();
+
+	/* 加载配置文件 */
+	void loadFile(const char* sPath);
+
+	/* 获取某行某列的值 */
+	Value getValue(int iRow, int iCol, const char* csvFilePath);
+
+	/* 获取某行某列的值，并转化为字符串 */
+	const std::string get(int iRow, int iCol, const char* csvFilePath);
+
+	/* 获取某行某列的值，并且转换为整型 */
+	const int getInt(int iRow, int iCol, const char* csvFilePath);
+
+	/* 获取某行某列的值，并且转换为浮点型 */
+	const float getFloat(int iRow, int iCol, const char* csvFilePath);
+
+	/* 获取某行某列的值，并且转换为布尔型 */
+	const bool getBool(int iRow, int iCol, const char* csvFilePath);
+
+	/* 获取配置文件数据的行和列数量 */
+	const Size getFileRowColNum(const char* csvFilePath);
+
+	/* 根据某个列的值，查找该值所在的行 */
+	const int findValueInWithLine(const char* chValue, int iValueCol, const char* csvFilePath);
+private:
+	static CsvUtil* m_CsvUtil;
+
+	/* 存放mCsvStrList-filePath的字典 */
+	Map<std::string, CsvData*> mCsvMap;
+};
+
+#endif
